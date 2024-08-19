a0a8f79dbae0e2a7eea7b4345ac55e9860fcc1f9
Ana Geguchadze <anageguchadze22@gmail.com>
2024-08-19 15:06:19 +0200

    sql

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+    author = models.CharField(max_length=200)
+
+class Person(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
